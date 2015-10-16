dab8efc1228f8220d8f7a9beae2af95982819efc
eraseee <erase@hotmail.dk>
2015-10-16 11:07:31 +0200

    basic movement of player

--- shooterGame/Assets/Script/PlayerMovement.cs
+++ shooterGame/Assets/Script/PlayerMovement.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
 	public float Verticalspeed = 10.0F;
-	public float HorizontalSpeed = 100.0F;
+	public float Horizontalspeed = 10.0F;
 
 	// Use this for initialization
 	void Start () {
@@ -14,12 +14,11 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 
 	void Update() {
-			float translation = Input.GetAxis("Vertical") * speed;
-			float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-			translation *= Time.deltaTime;
-			rotation *= Time.deltaTime;
-			transform.Translate(0, 0, translation);
-			transform.Rotate(0, rotation, 0);
+			float VerticalTranslation = Input.GetAxis("Vertical") * Verticalspeed;
+			float HorizontalTranslation = Input.GetAxis("Horizontal") * Horizontalspeed;
+			VerticalTranslation *= Time.deltaTime;
+			HorizontalTranslation *= Time.deltaTime;
+			transform.Translate(HorizontalTranslation, 0, 0);
+			transform.Translate(0, VerticalTranslation, 0);
 	}
-}
 }
