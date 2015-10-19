4481fd68145f7df5692732e3a6c732b06fac5a0c
BoOlsen <boolsen14@gmail.com>
2015-10-19 14:08:09 +0200

    Added the KILL SCRIPT!!!!

---
+++ shooterGame/Assets/Scripts/KillScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillScript : MonoBehaviour
+{
+    private Vector3 startPos;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    startPos = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	    if (transform.position.y < -10)
+	    {
+            transform.position = startPos;
+	    }
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            transform.position = startPos;
+        }
+    
+
+    }
+}
