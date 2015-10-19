e54b7293acda36e76739b0cff881b13c6cdd85c9
BoOlsen <boolsen14@gmail.com>
2015-10-19 12:04:20 +0200

    Worked furhter on the scripts

---
+++ shooterGame/Assets/Scripts/RollingObstacle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollingObstacle : MonoBehaviour {
+
+    private Vector3 rightPos;
+    private Vector3 leftPos;
+
+
+
+    // Use this for initialization
+    void Start () {
+        rightPos = this.transform.position;
+        leftPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+       // yield return StartCoroutine("MoveObjectDown");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
