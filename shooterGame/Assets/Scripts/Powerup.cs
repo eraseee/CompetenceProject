99ca838842800f17bba6bc29b757226ef3d164f6
eraseee <erase@hotmail.dk>
2015-10-19 14:13:32 +0200

    updated player prefab

---
+++ shooterGame/Assets/Scripts/Powerup.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+
+	bool machineGunUnlocked;
+
+	GameObject powerup;
+
+	// Use this for initialization
+	void Start () {
+		machineGunUnlocked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public bool GetMachineGun () {
+		return machineGunUnlocked;
+	}
+}
