01dcf89ec5c63a7977c1141e267af3764cf6645c
BoOlsen <boolsen14@gmail.com>
2015-10-20 10:05:33 +0200

    Changed the falling object, now they actually kill you.

--- shooterGame/Assets/Scripts/KillScript.cs
+++ shooterGame/Assets/Scripts/KillScript.cs
@@ -3,25 +3,32 @@ using System.Collections;
 
 public class KillScript : MonoBehaviour
 {
+    private CapsuleCollider caps;
 
 	// Use this for initialization
 	void Start ()
 	{
+        caps = GetComponent<CapsuleCollider>();
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
     {
 
-	}
+	    if (Physics.Raycast(transform.position, Vector3.up, caps.bounds.extents.y + 0.1f))
+	    {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+    }
 
     void OnCollisionEnter(Collision collision)
     {
+
         if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "KillingObstacle" || collision.gameObject.tag == "DeathTrigger")
         {
             Application.LoadLevel(Application.loadedLevel);
         }
-    
-
+   
     }
 }
